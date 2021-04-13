8bd63c7255608186fee89fd817c544573bb64e4c
Yu-Ting-Lee <taiwaneddy504@gmail.com>
2021-04-13 14:26:45 +0800

    20210413

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 var patt1 = new RegExp("^[a-z0-9]+$", "i");
 var patt2 = new RegExp("^[0-9]+$");
 
-var check = function (i, url) {
+var check = function (i) {
     let value = $("#reg" + i).val();
 
     if (value.length == 0) {
@@ -12,24 +12,6 @@ var check = function (i, url) {
     }
 
     switch (i) {
-        case 1:
-            let res;
-
-            $.ajax({
-                type: "POST",
-                url: url,
-                async: false,
-                data: { account: value },
-                success: function (data) {
-                    res = data;
-                }
-            });
-
-            if (res != "SUCCESS") {
-                $("#err1").html("*Account has been registered! QAQ");
-                return false;
-            }
-            break;
         case 2:
             if (!patt1.test(value)) {
                 $("#err2").html("*Invalid format (only upper/lower-case character and number are allowed)");
@@ -61,20 +43,36 @@ $(document).ready(function () {
     }
 
     $("#reg").submit(function (e) {
+        e.preventDefault();
+
         let url = $(this).attr("action");
+        let value = $("#reg1").val();
         let success = true;
 
-        for (let i = 1; i <= 4; i++) {
-            success &= check(i, url);
+        for (let i = 2; i <= 4; i++) {
+            success &= check(i);
         }
 
-        if (success) {
-            $(".nav-tabs a:first").tab("show");
-            for (let i = 1; i <= 4; i++) {
-                $("#reg" + i).val("");
-            }
+        if (value.length == 0) {
+            $("#err1").html("*Required!");
+        } else {
+            $.post({
+                url: url,
+                data: { 
+                    type: "register",
+                    account: value
+                },
+                success: function (data) {
+                    if(data == "SUCCESS" && success) {
+                        $(".nav-tabs a:first").tab("show");
+                        for (let i = 1; i <= 4; i++) {
+                            $("#reg" + i).val("");
+                        }
+                    } else {
+                        $("#err1").html("*Account has been registered! QAQ");
+                    }
+                }
+            });
         }
-
-        e.preventDefault();
     });
 });
